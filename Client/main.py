9343c0d684969283ccc6067fbab31ac18e89fdcf
khaluan <50902621+khaluan@users.noreply.github.com>
2021-11-15 01:26:13 +0700

    Initial commit

---
+++ Client/main.py
@@ -0,0 +1,15 @@
+import socket
+
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+from Config.config import *
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+print('Received', repr(data))
---
+++ Config/config.py
@@ -0,0 +1,4 @@
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 1337      # Port to listen on (non-privileged ports are > 1023)
+
+
